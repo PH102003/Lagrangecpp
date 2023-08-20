fb8c4239e08eaee7ff545d3492c76862e14c1483
PH102003 <pedroh.rj2003@hotmail.com>
2023-08-20 12:08:41 -0300

    add

--- lagrange.cpp
+++ lagrange.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <vector>
 
-double lagrange(){
+//criaçao de vetores double e do ponto de l1 (point)
+double lagrange(const std::vector<double>& x, const std::vector<double>& y, double point){
+    //variavel resultado
+    double r = 0.0;
+
+    double n = x.size();
+    for(int i = 0; i < n; i++){
+        //numero de yi
+        double yi = y[i];
+        for(int j = 0; j < n; j++){
+            if(j != i){
+                yi *= (point - x[j])/(x[i] - x[j]);
+            }
+        }
+    }
     return 0;
 }
